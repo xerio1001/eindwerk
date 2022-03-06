aec3264f9972d88e74320563968bc3e1e2c8360a
Dario Van Hasselt <dariovanhasselt@hotmail.be>
2022-03-06 13:15:21 +0100

    eindwerk initial upload

---
+++ eindwerk/main.py
@@ -0,0 +1,29 @@
+class Dots:
+    def __init__(self):
+        pass
+
+    def location(self):
+        pass
+
+    def thickness(self):
+        pass
+
+    def color(self):
+        pass
+
+    def reLocate(self):
+        pass
+
+
+class Polygon:
+    def triangle(self):
+        pass
+
+    def rectangle(self):
+        pass
+
+    def triangle(self):
+        pass
+
+    def reSize(self):
+        pass
