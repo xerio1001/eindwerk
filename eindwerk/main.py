9630a5378e69ae0a257bdbe8e7dca3937fb58813
Dario Van Hasselt <dariovanhasselt@hotmail.be>
2022-03-06 17:17:11 +0100

    Second Break

--- eindwerk/main.py
+++ eindwerk/main.py
@@ -1,9 +1,29 @@
+import turtle
+
+s = turtle.getscreen()
+turtle.title("Draw Polygons")
+t = turtle.Turtle()
+t.reset()
+
+
 class Dots:
     def __init__(self, x:int, y:int, thickness:int, color:str):
         self.x = x
         self.y = y
         self.thickness = thickness
-        self.color = color
+        self.color = color.lower()
+
+    def pickColor(self):
+        if(self.color == "red"):
+            t.color = "red"
+        elif(self.color == "green"):
+            t.color = "green"
+        elif(self.color == "blue"):
+            t.color = "blue"
+        elif(self.color == "black"):
+            t.color = "black"
+        else:
+            raise ValueError(f'You need to pick a color in the RGB range or black. (red, green, blue)')
 
     def reLocate(self):
         pass
@@ -19,8 +39,7 @@ class Triangle(Dots):
 class Rectangle(Dots):
     pass
 
-
-class Triangle(Dots):
+class Square(Dots):
     pass
 
 
---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+from eindwerk.main import *
+import pytest
+
+
+def test_validDotConstructor():
+    dotsDummy1 = Dots(1, 1, 2, "Red")
+    assert dotsDummy1.x == 1 and dotsDummy1.y == 1 and dotsDummy1.thickness == 2 and dotsDummy1.color == "red"
+    
