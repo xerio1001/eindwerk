ab21ebbbe4ca51af85b74f7249dbe86eee8e0016
xerio1001 <dariovanhasselt@hotmail.be>
2022-03-08 16:52:35 +0100

    Eindewerk

--- eindwerk/main.py
+++ eindwerk/main.py
@@ -1,46 +1,125 @@
 import turtle
 
-s = turtle.getscreen()
 turtle.title("Draw Polygons")
 t = turtle.Turtle()
-t.reset()
-
 
 class Dots:
-    def __init__(self, x:int, y:int, thickness:int, color:str):
-        self.x = x
-        self.y = y
-        self.thickness = thickness
-        if(self.isValidColor(color)):
-            self.color = color.lower()
+    def __init__(self, x:int, y:int):
+        if type(x) is not int:
+            raise TypeError(f'The value of the x variable is not an integer. {x}')
         else:
-            raise ValueError(f'You need to pick a color within the range of RGB values (red, green, blue) or black')
-
-    def isValidColor(self, color):
-        color = color.lower()
-        if(color == "red" or color == "green" or color == "blue" or color == "black"):
-            return True
-        else:
-            return False
+            self.x = x
         
+        if type(y) is not int:
+            raise TypeError(f'The value of the y variable is not an integer. {y}')
+        else:
+            self.y = y
 
     def reLocate(self):
         pass
 
+
+class Triangle:
+    def __init__(self, *args):
+        self.dots = args
+
+    def isValidColor(self, color):
+        color = color.lower()
+        if color == "red" or color == "green" or color == "blue" or color == "black":
+            return True
+        else:
+            return False
+
+    def draw(self, color, thickness, test:bool = False):
+        if self.isValidColor(color):
+            self.color = color.lower()
+        else:
+            raise ValueError(f'Give a color within the range of RGB or the color "black".')
+        
+        if thickness <= 0:
+            raise ValueError(f'The thickness of the line cannot be lower than or equal to 0')
+        else:
+            self.thickness = thickness
+
+        if test == False:
+            t.penup()
+            t.color(self.color)
+            t.pensize(self.thickness)
+            for dot in self.dots:
+                t.goto(dot.x, dot.y)
+                t.pendown()
+
+            t.goto(self.dots[0].x, self.dots[0].y)
+
+
+class Square:
+    def __init__(self, *args, length):
+        self.dots = args
+        self.length = length
+        self.getCorners()
+
+    def isValidColor(self, color):
+        color = color.lower()
+        if color == "red" or color == "green" or color == "blue" or color == "black":
+            return True
+        else:
+            return False
+
+    def getCorners(self):
+        p1 = self.dots[0] # Example: Bottom left
+        p2 = Dots(p1.x + self.length, p1.y) # Example: Bottom right
+        p3 = Dots(p1.x + self.length, p1.y + self.length) # Example: Top right
+        p4 = Dots(p1.x, p1.y + self.length) # Example: Top left
+
+        self.dots = list(self.dots)
+        self.dots.clear()
+        self.dots.extend((p1, p2, p3, p4))
+
+    def draw(self, color, thickness):
+        if self.isValidColor(color):
+            self.color = color.lower()
+        else:
+            raise ValueError(f'Give a color within the range of RGB or the color "black".')
+        
+        if thickness <= 0:
+            raise ValueError(f'The thickness of the line cannot be lower than or equal to 0')
+        else:
+            self.thickness = thickness
+
+        t.color(self.color)
+        t.pensize(self.thickness)
+        for dot in self.dots:
+            t.goto(dot.x, dot.y)
+            t.pendown()
+        t.goto(self.dots[0].x, self.dots[0].y)
+
     def reSize(self):
-        return NotImplemented
+        pass
 
 
-class Triangle(Dots):
-    pass
+class Rectangle:
+    def __init__(self, *args):
+        self.dots = args
+
+    def reSize(self):
+        pass
 
 
-class Rectangle(Dots):
-    pass
+class Polygon:
+    def __init__(self):
+        pass
 
-class Square(Dots):
-    pass
+#turtle.mainloop()
 
 
-class Polygon(Dots):
-    pass
+"""
+In het tekenprogramma is het de bedoeling dat men punten kan plaatsen.
+En willekeurige veelhoeken kan tekenen door meerdere punten te plaatsen. Alle veelhoeken hebben een lijndikte en kleur. Deze worden gebruikt in de draw method.
+Rechthoeken kan tekenen door 1 punt te plaatsen en een hoogte en breedte mee te geven.  Of door 2 punten te tekenen.
+Driehoeken kan tekenen.
+En vierkanten kan tekenen door 1 punt en een lengte mee te geven.
+
+Alles kan verplaatst worden door een verplaatsing in de x en y richting mee te geven.
+Rechthoeken en vierkanten kunnen resized worden door een hoekpunt te verslepen. Dit doe je door het oud hoekpunt en het nieuwhoek punt mee te geven.  Tip: Hier kan je hoekpunten vergelijken met == bijv.
+Alles kan geroteerd worden door een punt mee te geven en een hoek in graden. Dit zorgt voor een rotatie van je veelhoek/punt met die bepaalde hoek rond dat punt.
+"""
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,105 @@
-from multiprocessing.sharedctypes import Value
+from multiprocessing.managers import ValueProxy
 from eindwerk.main import *
 import pytest
 
 
-def test_validDotConstructor():
-    dotsDummy1 = Dots(1, 1, 2, "Red")
-    assert dotsDummy1.x == 1 and dotsDummy1.y == 1 and dotsDummy1.thickness == 2 and dotsDummy1.color == "red"
+#################### Dots Class Tests ####################
+
+@pytest.fixture
+def dotDummy1():
+    """Creates a dummy for dots with correct values."""
+    return Dots(10, 20)
     
 
-def test_invalidColor():
+def test_isValidDotsConstructor(dotDummy1):
+    dotDummy1
+    assert dotDummy1.x == 10 and dotDummy1.y == 20
+
+
+@pytest.mark.parametrize(("x", "y"),[
+    ("1", 2),
+    (1, "2"),
+    ("1", "2")
+])
+def test_isInvalidDotsConstructor(x, y):
+    with pytest.raises(TypeError):
+        Dots(x, y)
+
+#################### End Dots Class Tests ####################
+
+#################### Triangle Class Tests ####################
+
+@pytest.fixture
+def triangleDummy1():
+    """creates a dummy for triangle with correct values."""
+    p1 = Dots(10, 20)
+    p2 = Dots(30, 40)
+    p3 = Dots(50, 20)
+    return Triangle(p1, p2, p3)
+
+def test_isValidTriangleConstructor(triangleDummy1):
+    assert triangleDummy1.dots[0].x == 10 and triangleDummy1.dots[0].y == 20
+    assert triangleDummy1.dots[1].x == 30 and triangleDummy1.dots[1].y == 40
+    assert triangleDummy1.dots[2].x == 50 and triangleDummy1.dots[2].y == 20
+
+
+def test_areValidArguments(triangleDummy1):
+    triangleDummy1.draw("blue", 5, True)
+    assert triangleDummy1.color == "blue" and triangleDummy1.thickness == 5 
+
+
+@pytest.mark.parametrize(("color", "thickness"),[
+    ("orange", 5),
+    ("white", 3)
+])
+def test_isInvalidColor(triangleDummy1, color, thickness):
     with pytest.raises(ValueError):
-        Dots(1, 1, 2, "Orange")
+        triangleDummy1.draw(color, thickness)
+
+
+@pytest.mark.parametrize(("color", "thickness"),[
+    ("red", -2),
+    ("Blue", 0),
+])
+def test_correctValueOfThickness(triangleDummy1, color, thickness):
+    with pytest.raises(ValueError):
+        triangleDummy1.draw(color, thickness)
+
+#################### End Triangle class Tests ####################
+
+#################### Square Class Tests ####################
+
+@pytest.fixture
+def squareDummy1():
+    """creates a dummy for square with correct values."""
+    p1 = Dots(10, 20)
+    return Square(p1, length = 20)
+
+
+def test_isValidSquareConstructor(squareDummy1):
+    assert squareDummy1.dots[0].x == 10 and squareDummy1.dots[0].y == 20 and squareDummy1.length == 20
+
+
+def test_isValidColorAndThick(squareDummy1):
+    squareDummy1.draw("Red", 2)
+    assert squareDummy1.color == "red" and squareDummy1.thickness == 2
+
+
+@pytest.mark.parametrize(("color", "thickness"),[
+    ("Orange", 2),
+    ("white", 5)
+])
+def test_isInvalidColor(squareDummy1, color, thickness):
+    with pytest.raises(ValueError):
+        squareDummy1.draw(color, thickness)
+
+
+@pytest.mark.parametrize(("color", "thickness"),[
+    ("red", -7),
+    ("Green", 0)
+])
+def test_isInvalidThickness(squareDummy1, color, thickness):
+    with pytest.raises(ValueError):
+        squareDummy1.draw(color, thickness)
+
+#################### End Square Class Tests ####################
